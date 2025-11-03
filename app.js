8ea094666033a60152c727d011ac7e25ac63914c
Nigga <traviscott075657@gmail.com>
2025-11-04 00:16:41 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let eyeicon = document.getElementById("eyeicon");
+let password = document.getElementById("password");
+
+eyeicon.onclick = function(){
+    if(password.type == "password"){
+        password.type = "text"
+        eyeicon.src = "eyeopen.png"
+    }else{
+        password.type = "password"
+        eyeicon.src = "eyeclose.png"
+    }
+}
